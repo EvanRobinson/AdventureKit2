b9bb6bc2bc8e10fab3211074e9fca4d5cb016636
EvanRobinson <evanrobinson@mac.com>
2023-10-10 14:03:07 -0600

    Ch 01 Day 03 Easy Light Toggles, add gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,58 @@
 #include <Arduino.h>
 
 const uint8_t whiteLEDControlPin = 22;
+const uint8_t buttonInputPin = 24;
 const int ledOnIndefintely = 0;
+const long debounceInterval = 50L;
+const int buttonPressed = 1;
 
 // put function forward declarations here:
 void turnLEDOnFor(int millis = 1000, uint8_t pin = whiteLEDControlPin);
 void turnLEDOff(uint8_t pin = whiteLEDControlPin);
+void toggleInteriorLights(void);
 
 void setup() {
   pinMode(whiteLEDControlPin, OUTPUT);
+  turnLEDOff();
+  pinMode(buttonInputPin, INPUT);
+
+  Serial.begin(9600);
+  Serial.println("setup complete");
 }
 
-#define THROW_AWAY_CODE
 
 void loop() {
-#ifdef THROW_AWAY_CODE
-  digitalWrite(whiteLEDControlPin, HIGH);
-  delay(1000);
-  digitalWrite(whiteLEDControlPin, LOW);
-  delay(1000);
-#else
-  turnLEDOnFor(300);
-  delay(300);
-#endif
+  static int previousPinValue = 0;
+  static long previousDebounceTime = millis();
+  int pinValue = digitalRead(buttonInputPin);
+
+  if ((millis() - previousDebounceTime) < debounceInterval) {
+    // debouncing: if the transition was less than debounceInterval ms ago, ignore it
+    return;
+  }
+
+  if (previousPinValue != pinValue) {
+    previousPinValue = pinValue;
+    if (pinValue == buttonPressed) {
+      toggleInteriorLights();
+    }
+  }
 }
 
 // put function definitions here:
+void toggleInteriorLights(void) {
+  static bool lightIsOn = false;
+
+  if (lightIsOn) {
+    turnLEDOff();
+    lightIsOn = false;
+  }
+  else {
+    turnLEDOnFor(ledOnIndefintely);
+    lightIsOn = true;
+  }
+}
+
 void turnLEDOnFor(int millis, uint8_t pin) {
   digitalWrite(pin, HIGH);
   if (millis != ledOnIndefintely) {
